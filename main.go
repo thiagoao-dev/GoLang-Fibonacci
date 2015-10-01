9ffe328bae135e09359a1fb498b205f32ecb442b
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2015-10-01 18:44:14 +0000

    Simple fibonacci implementation

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+var last int
+var now  int
+
 func main() {
-  fmt.Println("test")
+
+  last = 0
+  now  = 1
+  
+  for i := 1; i < 10; i++{
+    fmt.Printf("%d\n",fibonacci())    
+  }
+
+}
+
+func fibonacci() int {
+  
+  total := last + now
+  now = last
+  last = total
+  
+  return total
 }
