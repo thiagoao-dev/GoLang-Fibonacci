1b3c9030015a98bc8e1ae07ede51b74e62297e80
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2015-10-06 18:47:10 +0000

    Fix main

--- algorithm/fibonacci.go
+++ algorithm/fibonacci.go
@@ -2,10 +2,12 @@ package algorithm
 
 // fibonacci is a function that returns
 // a function that returns an int.
-func Fibonacci() func(int) int {
-	sum := 0
-	return func (x int) int {
-     sum += x
-     return sum
+func Fibonacci() func() int {
+	sum, before, next := 0, 0, 1
+	return func() int {
+		sum    = before
+		before = next
+		next   = before + sum
+		return sum
 	}
 }
