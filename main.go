b348a2715a0d485c34b98808b6ff0d546b173bc9
Thiago Augustus <thiagoaugustusdeoliveira@gmail.com>
2015-10-11 23:04:32 -0300

    Add rand of 20

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "github.com/thiagoaugustus/GoLang-Fibonacci/algorithm"
+	"fmt"
+	"github.com/thiagoaugustus/GoLang-Fibonacci/algorithm"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	f := algorithm.Fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < rand.Intn(20); i++ {
+		fmt.Println(i, f())
 	}
 }
